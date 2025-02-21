21653c8fdb27eed89016443271311839a822c59d
adude <adwaidhbabu007@gmail.com>
2025-02-21 12:05:20 +0000

    Order Invoice

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -82,6 +82,7 @@ router.post('/verify-payment',userAuth.isLogged,userAuth.isBlocked,checkoutContr
 router.get("/myAccount/order/:id",orderController.loadOrderDetails);
 router.post("/myAccount/order/cancel/:id",orderController.cancelOrder);
 router.get("/order/success",orderController.orderSuccess);
+router.get("/order/downloadinvoice/:id",orderController.downloadInvoice);
 
 
 //Coupon Controller
