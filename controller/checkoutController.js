fea25538e42ae8b416ba30ee04bf3bb17e487e9a
adude <adwaidhbabu007@gmail.com>
2025-03-05 03:41:25 +0000

    apply coupon bug

--- controller/checkoutController.js
+++ controller/checkoutController.js
@@ -6,6 +6,7 @@ const Coupon = require("../model/couponSchema");
 const Wallet = require("../model/walletSchema");
 const Razorpay = require('razorpay');
 var { validatePaymentVerification, validateWebhookSignature } = require('razorpay/dist/utils/razorpay-utils');
+const { readInt16 } = require("pdfkit/js/data");
 const checkoutController = {};
 
 
@@ -48,6 +49,10 @@ checkoutController.loadCheckout = async (req, res) => {
             // Add the price of the variant * quantity to the total
             return total + (variant.salePrice * element.quantity);
         }, 0); // Initial value is 0
+
+        if (req.session.totalPrice !== totalPrice) {
+            req.session.totalPrice = totalPrice
+        }
 
         //If cart.coupoon applied is true then apply the discount percent to each product other then add find the total 
 
